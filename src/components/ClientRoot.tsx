1e690cc8e49d6f9317e7cedb0f71e2db8ab7e82d
yudikuse <marcelokuse@gmail.com>
2025-09-01 10:50:42 -0300

    Create ClientRoot.tsx

---
+++ src/components/ClientRoot.tsx
@@ -0,0 +1,9 @@
+// src/components/ClientRoot.tsx
+"use client";
+
+import CookieBanner from "@/components/CookieBanner";
+
+export default function ClientRoot() {
+  // Tudo que é client-only entra aqui
+  return <CookieBanner />;
+}
