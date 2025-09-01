d2e2f33ef98136c7c3b1c08abfb0b3fe6a3d01e8
yudikuse <marcelokuse@gmail.com>
2025-09-01 08:29:04 -0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  async redirects() {
+    return [
+      { source: '/termos', destination: '/terms', permanent: true },
+      { source: '/privacidade', destination: '/privacy', permanent: true },
+    ];
+  },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
