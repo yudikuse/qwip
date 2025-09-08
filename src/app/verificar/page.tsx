4ac6ac110ba9f462cda8c5583ceee8a1169ce871
yudikuse <marcelokuse@gmail.com>
2025-09-08 13:44:20 -0300

    Update page.tsx

--- src/app/verificar/page.tsx
+++ src/app/verificar/page.tsx
@@ -1,7 +1,9 @@
 // src/app/verificar/page.tsx
 import { Suspense } from "react";
 import VerifyClient from "./verify-client";
+
 export const dynamic = "force-dynamic";
+
 export default function Page() {
   return (
     <Suspense>
