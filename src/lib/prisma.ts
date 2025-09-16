58b67c3ac93f666b1d108c5f25d232b0f3ce1ba4
yudikuse <marcelokuse@gmail.com>
2025-09-16 13:24:15 -0300

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -6,7 +6,7 @@ const globalForPrisma = globalThis as unknown as { prisma?: PrismaClient };
 export const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    log: process.env.NODE_ENV === "development" ? ["query", "error", "warn"] : ["error"],
+    // log: ["query", "error", "warn"], // habilite se quiser inspecionar no dev
   });
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
