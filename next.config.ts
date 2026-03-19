9e0ad65ab00fe19b69a33d6191a11cd434222296
yudikuse <marcelokuse@gmail.com>
2026-03-19 13:48:20 -0300

    Add files via upload

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,3 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  async redirects() {
-    return [
-      // Mata definitivamente o fluxo antigo:
-      { source: '/anuncio/novo', destination: '/anunciar', permanent: true },
-      { source: '/anuncio/criar', destination: '/anunciar', permanent: true },
-      { source: '/anunciar/novo', destination: '/anunciar', permanent: true },
-    ];
-  },
-};
-
+const nextConfig = {};
 module.exports = nextConfig;
